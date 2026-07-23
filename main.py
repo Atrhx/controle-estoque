41e6aeaba781b62096b8ab8a5fb03fdbc6823f22
Arthur Guilherme <arthurguilhermegsds@gmail.com>
2026-07-23 10:57:51 -0300

    Adiciona cadastro e listagem de produtos

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
-print("="*3, "CONTROLE DE ESTOQUE", "="*3)
-print("\n1. Cadastrar produto")
-print("2. Listar produtos")
-print("3. Sair\n")
-print("="*28)
+import time
 
-acao = input("Escolha uma opção: ")
-print(f"Você escolheu: {acao}")
+produtos_estoque = []
+
+def cadastrar_produtos():
+    descricao_produto = input("Descrição do item:")
+    codigo = input("Código do item:")
+    quantidade = int(input("Quantidade:"))
+    produto_cadastrado = {"nome": descricao_produto, "id": codigo, "quantidade": quantidade}
+    produtos_estoque.append(produto_cadastrado)
+
+def listar_produtos():
+    for produto in produtos_estoque:
+        print(f"Descrição: {produto['nome']}")
+        print(f"ID: {produto['id']}")
+        print(f"Quantidade: {produto['quantidade']}")
+
+while True:
+    print("="*3, "CONTROLE DE ESTOQUE", "="*3)
+    print("\n1. Cadastrar produto")
+    print("2. Listar produtos")
+    print("3. Sair\n")
+    print("="*28)
+    acao = input("Escolha uma opção: ")
+
+    if acao == "1":
+        cadastrar_produtos()
+    elif acao == "2":
+        listar_produtos()
+    elif acao == "3":
+        print("Encerrando o programa...")
+        print("Salvando os produtos")
+        time.sleep(5)
+        break
+    else:
+        print("Opção inválida.")
